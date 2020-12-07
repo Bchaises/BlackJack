2f03dade4a11e7d67dd13a07754e24b777f9de6d
Bchaises <benjamin.chaises@gmail.com>
2020-12-07 20:23:19 +0100

    commentaires

--- Controller/index.php
+++ Controller/index.php
@@ -1,30 +1,43 @@
 <?php
+
+// On ajoute les deux DAO 
 require_once('../Model/DAO_Player.php');
 require_once('../Model/DAO_Game.php');
 
+// pour utiliser $_SESSION
 session_start();
 
+// la variable module gere les pages affiché
 $module = 'connection';
+
+// les messages permettent d'informer l'utilisateur, ils sont affiché en fin de page
 $messageErreur = '';
 $message = '';
 
+// on crée deux objets 
 $daoPlayer = new DAO_Player;
 $daoGame = new DAO_Game;
 
 // Connexion du joueur
+// vérification si le bouton a été utilisé
 if (isset($_POST['btnConnection'])) {
 
+	// on vérifie que l'utilisateur a bien rentré les informations correctement
 	if (isset($_POST['pseudo']) && $_POST['pseudo'] != '' && isset($_POST['password']) && $_POST['password'] != '') {
 		
+		// on utilise la fonction connectPlayer pour connecter le joueur
 		$res = $daoPlayer->connectPlayer($_POST['pseudo'], $_POST['password']);
 
+		// si res renvoie true alors affichage d'un message d'erreur
 		if ($res != true) {
 
 			$messageErreur = $res;
+			var_dump($messageErreur);
 		}
 	}
 	else
 	{
+		// s'affiche si la connection resulte d'une erreur
 		$messageErreur = "Le formulaire n'a pas été correctement remplis !";
 	}
 }
@@ -37,10 +50,13 @@ if (isset($_GET['inscription'])) {
 // inscription de l'utilisateur
 if (isset($_POST['btnInscription'])) {
 
+	// on vérifie que le formulaire d'inscription est correctement remplie
 	if (isset($_POST['pseudo']) && isset($_POST['password']) && $_POST['pseudo'] != '' && $_POST['password'] != '') {
 
+		// on ajoute le joueur avec la fonction addPlayer si l'utilisateur n'existe pas déjà
 		if ($daoPlayer->addPlayer($_POST['pseudo'],$_POST['password'],1000)) {
 
+			// on connecte le joueur
 			$res =  $daoPlayer->connectPlayer($_POST['pseudo'], $_POST['password']);
 		}
 		else
@@ -68,7 +84,7 @@ if (isset($_GET['deco'])) {
 // bouton continuer
 if (isset($_GET['continuer'])) {
 	header('location:index.php');
-	$module = 'continuer';
+	$module = "continuer";
 }
 
 // bouton arrêt
@@ -80,13 +96,18 @@ if (isset($_POST['btnStop'])) {
 // Mise
 if (isset($_POST['btnMise'])) {
 
+	// on vérifie si la mise est correcte
 	if (isset($_POST['mise']) && $_POST['mise'] >= 2 && $_POST['mise'] <= 100 && $_POST['mise'] <= $_SESSION['money']) {
 
+		// on enregistre la mise et on modifie l'argent du joueur
 		$_SESSION['mise'] = $_POST['mise'];
 		$_SESSION['money'] = ($_SESSION['money'] - $_SESSION['mise']);
 		$daoPlayer->updatePlayer($_SESSION['pseudo'],$_SESSION['money']);
 
+		// on lance le module distribCartes
 		$module = 'distribCartes';
+
+	// message d'erreurs différents
 	}else if ($_POST['mise'] < 0) {
 		$messageErreur = "Erreur vous avez saisie une valeur négative.";
 	}else if ($_POST['mise'] > 100) {
@@ -101,66 +122,98 @@ if (isset($_POST['btnMise'])) {
 // Choix du joueur
 if (isset($_POST['btnChoiceSubmit'])) {
 
+	// on vérifie qu'un bouton a été selectionné
 	if (isset($_POST['radioChoice']) && $_SESSION['valueP']<21) {
 
+		// si le bouton draw/tiré une carte a été selectionné
 		if ($_POST['radioChoice'] == "draw" ) {
 
+			// on vérifie si la carte existe
 			$nbrRand = rand(1,52);
 			while ($daoGame->verifyCard($nbrRand,$_SESSION['useCards']) == true) {
 				$nbrRand = rand(1,52);
 			}
 
+			// on donne alors la valeur de la carte, on l'ajoute dans le nombre de carte du joueur et on ajoute une session avec la carte
 			$_SESSION['valueP'] += ($daoGame->valueCard($nbrRand));
 			$_SESSION['nbrP'] += 1;
 			$_SESSION['carteP'.$_SESSION['nbrP']] = $nbrRand;
+
+			// on met la carte d'un le tableau usecards 
 			array_push($_SESSION['useCards'], $nbrRand);
 
+			// si le joueur a une valeur de plus de 21 avec la carte tiré alors la partie est terminé
 			if ($_SESSION['valueP'] > 21 || $_SESSION['valueP'] == 21) {
 				$module = 'finPartie';
+			//sinon on redirige le joueur dans le module game
 			}else{
 				$module = 'game';
 			}
 		}
 
+		// si le bouton past/passé la main a été selectionné
 		if ($_POST['radioChoice'] == 'past') {
 			
+			// c'est le tour du croupier
+			// on vérifie que la carte existe
 			$nbrRand = rand(1,52);
 			while ($daoGame->verifyCard($nbrRand,$_SESSION['useCards']) == true) {
 			$nbrRand = rand(1,52);
 			}
+
+			// on change la carte retourné
 			$_SESSION['carteC1'] = $nbrRand;
 			array_push($_SESSION['useCards'], $nbrRand);
 
+			// tant que la valeur des cartes du croupier n'est pas égale ou superieur a 16 alors il pioche
 			while ($_SESSION['valueC'] < 16) {
+
+				// on vérifie si la carte existe
 				$nbrRand = rand(1,52);
 				while ($daoGame->verifyCard($nbrRand,$_SESSION['useCards']) == true) {
 				$nbrRand = rand(1,52);
 				}
+
+				// on ajoute dans le nombre de cartes du croupier, on ajoute une session avec la carte et la valeur de celle ci
 				$_SESSION['nbrC'] += 1;
 				$_SESSION['carteC'.$_SESSION['nbrC']] = $nbrRand;
 				$_SESSION['valueC'] += ($daoGame->valueCard($nbrRand));
+				// on l'ajoute dans usecards
 				array_push($_SESSION['useCards'], $nbrRand);
 			}
 
+			// à la fin du tour du croupier c'est la fin du jeu
 			$module = 'finPartie';
 		}
 
+		// si le bouton double/ double son jeu est selectionné
 		if ($_POST['radioChoice'] == 'double') {
 			
+			// ajout d'une carte puis double la mise
+			// on vérifie si la carte existe
 			$nbrRand = rand(1,52);
 			while ($daoGame->verifyCard($nbrRand,$_SESSION['useCards']) == true) {
 				$nbrRand = rand(1,52);
 			}
 
+			// on ajoute valeur, nombre et carte
 			$_SESSION['valueP'] += ($daoGame->valueCard($nbrRand));
 			$_SESSION['nbrP'] += 1;
 			$_SESSION['carteP'.$_SESSION['nbrP']] = $nbrRand;
+
+			// on l'ajoute dans le tableau usecards
 			array_push($_SESSION['useCards'], $nbrRand);
 
+			// on modifie la mise en la multipliant pas deux
 			$_SESSION['mise'] *= 2;
+
+			// on modifie le solde du joueur
 			$_SESSION['money'] = ($_SESSION['money'] - $_SESSION['mise']);
+
+			// le solde du joueur est modifier dans la base de données
 			$daoPlayer->updatePlayer($_SESSION['pseudo'],$_SESSION['money']);
 
+			// si la valeur est supérieur ou égale a 21 alors fin de partie
 			if ($_SESSION['valueP'] > 21 || $_SESSION['valueP'] == 21) {
 				$module = 'finPartie';
 			}else{
@@ -168,44 +221,83 @@ if (isset($_POST['btnChoiceSubmit'])) {
 			}
 		}
 
+	// si la valeur est superieur ou inferieur a 21 alors fin de partie
 	}else if ($_SESSION['valueP'] == 21 || $_SESSION['valueP'] > 21) {
 				$module= 'finPartie';
+	// erreur si le joueur n'a pas selectionné de choix
 	}else{
 		$messageErreur="Veuillez faire un choix parmis les quatres proposés pour continuer.";
+		$module = 'game';
 	}
 }
 
+// module fin de partie
 if ($module == 'finPartie') {
+
+	// perdu si la valeur du joueur est superieur à 21
 	if ($_SESSION['valueP']>21) {
+
+		// on informe le joueur de la fin de partie ainsi que de la somme perdu
 		$message = 'Vous avez perdu. Votre mise de '.$_SESSION['mise'].'€ a été retiré. Merci d\'avoir joué.';
 	}
+	// blackjack si la valeur du joueur est égale à 21
 	else if ($_SESSION['valueP'] == 21) {
+
+		// on informe le joueur de la victoire
 		$message = 'BlackJack! Vous récupérez 2 fois votre mise.';
+
+		// on modifie le solde du joueur, il a gagné 2 fois la mise
 		$daoPlayer->updatePlayer($_SESSION['pseudo'], ($_SESSION['money'] + ($_SESSION['mise'] * 2) ));
 		$_SESSION['money'] += $_SESSION['mise'] * 2; 
+
+	// si la valeur du croupier est superieur a 21
 	}else if ($_SESSION['valueC']>21) {
+
+		// on informe le joueur de la victoire
 		$message = 'Vous avez gagné! Vous récupérez donc votre mise plus 1x votre mise.';
+
+		// on modifie le solde 
 		$daoPlayer->updatePlayer($_SESSION['pseudo'], ($_SESSION['money'] + ($_SESSION['mise'] * 2) ));
 		$_SESSION['money'] += $_SESSION['mise'] * 2;
+
+	// si le croupier a un blackjack
 	}else if ($_SESSION['valueC']==21) {
+
+		// on informe le joueur de la défaite
 		$message = 'Vous avez perdu. Votre mise de '.$_SESSION['mise'].'€ a été retiré. Merci d\'avoir joué.';
 	}
+	// si le joueur a une valeur superieur au croupier
 	else if ($_SESSION['valueP'] > $_SESSION['valueC']) {
+
+		// on informe le joueur de la victoire
 		$message = 'Vous avez gagné! Vous récupérez donc votre mise plus 1x votre mise.';
+
+		// on modifie le solde
 		$daoPlayer->updatePlayer($_SESSION['pseudo'], ($_SESSION['money'] + ($_SESSION['mise'] * 2) ));
 		$_SESSION['money'] += $_SESSION['mise'] * 2; 
 	}
+	// si la valeur du croupier est superieur a celle du joueur
 	else if ($_SESSION['valueC'] > $_SESSION['valueP']) {
+
+		// on informe le joueur de la défaite
 		$message = 'Vous avez perdu. Votre mise de '.$_SESSION['mise'].'€ a été retiré. Merci d\'avoir joué.';
 	}
+	// si c'est une égalité
 	else{
+
+		// on informe le joueur de l'égalité
 		$message = "C'est une égalité! vous récupérez votre mise.";
+
+		// la mise est récupéré
 		$daoPlayer->updatePlayer($_SESSION['pseudo'], ($_SESSION['money'] + $_SESSION['mise']));
 		$_SESSION['money'] += $_SESSION['mise']; 
 	}
 }
 
+// module continuer
 if ($module == 'continuer') {
+
+	// on unset toutes les variables session sauf celle de la connexion
 	for ($i=0; $i < $_SESSION['nbrP']; $i++) { 
 			unset($_SESSION['carteP'.$i]);
 		}
@@ -214,121 +306,88 @@ if ($module == 'continuer') {
 		}
 		unset($_SESSION['nbrP'], $_SESSION['nbrC'], $_SESSION['useCards'], $_SESSION['valueP'], $_SESSION['valueC']);
 
+		// on retourne sur le module connection
 		$module = 'connection';
 }
 
 // distribution des cartes pour le croupier et le joueur
 if ($module == 'distribCartes') {
-	$cartes = array(
-		1 => 'As_Trèfle',
-		2 => 'As_Pique',
-		3 => 'As_Coeur',
-		4 => 'As_Carreau',
-		5 => 'Roi_Trèfle',
-		6 => 'Roi_Pique',
-		7 => 'Roi_Coeur',
-		8 => 'Roi_Carreau',
-		9 => 'Dame_Trèfle',
-		10 => 'Dame_Pique',
-		11 => 'Dame_Coeur',
-		12 => 'Dame_Carreau',
-		13 => 'Valet_Trèfle',
-		14 => 'Valet_Pique',
-		15 => 'Valet_Coeur',
-		16 => 'Valet_Carreau',
-		17 => '10_Trèfle',
-		18 => '10_Pique',
-		19 => '10_Coeur',
-		20 => '10_Carreau',
-		21 => '9_Trèfle',
-		22 => '9_Pique',
-		23 => '9_Coeur',
-		24 => '9_Carreau',
-		25 => '8_Trèfle',
-		26 => '8_Pique',
-		27 => '8_Coeur',
-		28 => '8_Carreau',
-		29 => '7_Trèfle',
-		30 => '7_Pique',
-		31 => '7_Coeur',
-		32 => '7_Carreau',
-		33 => '6_Trèfle',
-		34 => '6_Pique',
-		35 => '6_Coeur',
-		36 => '6_Carreau',
-		37 => '5_Trèfle',
-		38 => '5_Pique',
-		39 => '5_Coeur',
-		40 => '5_Carreau',
-		41 => '4_Trèfle',
-		42 => '4_Pique',
-		43 => '4_Coeur',
-		44 => '4_Carreau',
-		45 => '3_Trèfle',
-		46 => '3_Pique',
-		47 => '3_Coeur',
-		48 => '3_Carreau',
-		49 => '2_Trèfle',
-		50 => '2_Pique',
-		51 => '2_Coeur',
-		52 => '2_Carreau',
-		53 => 'dos',
-	);
 
-	$carteP = '';
-	$carteC = '';
+	// on initialise les variables session du nombre de cartes pour le croupier et le joueur
+	// ainsi que les variables de valeurs des cartes
 	$_SESSION['nbrP'] = 0;
 	$_SESSION['nbrC'] = 0;
+	$_SESSION['valueP'] = 0;
+	$_SESSION['valueC'] = 0;
+
+	// on initialise le tableau des cartes utilisés
 	$_SESSION['useCards'] = array();
 
 
-	// carte du joueur
+	// donne 2 cartes au joueur
 	$nbrRand = rand(1,52);
 	$_SESSION['carteP1'] = $nbrRand;
-	$_SESSION['valueP'] = 0;
-	$_SESSION['valueC'] = 0;
 	$_SESSION['valueP'] += ($daoGame->valueCard($nbrRand));
 	$_SESSION['nbrP'] += 1;
+
+	// on met la carte dans le tableau des cartes utilisés
 	array_push($_SESSION['useCards'], $nbrRand);
 
+	// vérification si la carte existe
 	$nbrRand = rand(1,52);
 	while ($daoGame->verifyCard($nbrRand,$_SESSION['useCards']) == true) {
 		$nbrRand = rand(1,52);
 	}
+
+	// donne une deuxieme carte
 	$_SESSION['carteP2'] = $nbrRand;
 	$_SESSION['valueP'] += ($daoGame->valueCard($nbrRand));
 	$_SESSION['nbrP'] += 1;
+
+	// on le met dans le tableau des cartes utilisées
 	array_push($_SESSION['useCards'], $nbrRand);
 
 	// carte du croupier
+	// on vérifie si la carte existe
 	$nbrRand = rand(1,52);
 	while ($daoGame->verifyCard($nbrRand,$_SESSION['useCards']) == true) {
 		$nbrRand = rand(1,52);
 	}
+
+	// on met la carte retourné
 	$_SESSION['carteC1'] = 53;
 	$_SESSION['nbrC'] += 1;
+
+	// on met la deuxieme carte
 	$_SESSION['carteC2'] = $nbrRand;
 	$_SESSION['valueC'] += ($daoGame->valueCard($nbrRand));
 	$_SESSION['nbrC'] += 1;
+
+	// on met la carte dans le tableau des cartes utilisées
 	array_push($_SESSION['useCards'], $nbrRand);
 }
 
 // affichage des cartes
 if ($module == 'distribCartes' || $module == 'game' || $module == 'finPartie') {
+
+	// affichage des images du joueur
 	$affichageP = '';
 	for ($i=1; $i <= $_SESSION['nbrP']; $i++) {
 		$affichageP .= '<img src="../Images/'.$_SESSION['carteP'.$i].'.png">';
 	}
 
+	// affichage des images du croupier
 	$affichageC = '';
 	for ($i=1; $i <= $_SESSION['nbrC']; $i++) { 
 		$affichageC .= '<img src="../Images/'.$_SESSION['carteC'.$i].'.png">';
 	}
 }
 
+// page html
 include('../Vue/top_page.php');
 include('../Vue/header.php');
 
+// différents modules
 if ($module == 'connection') {
 	include('../Vue/lien_inscription.php');
 	include('../Vue/connection_form.php');
--- Vue/choix.php
+++ Vue/choix.php
@@ -2,23 +2,23 @@
 	<table class="tableChoice">
 		<tr class="trTableChoice1">
 			<td>
-				<input type="radio" name="radioChoice" id="draw" value="draw" class="drawCard">
-				<label name="drawCard">Tirer une carte</label>
+				<input type="radio" name="radioChoice" id="draw" value="draw" class="drawCard" required>
+				<label name="drawCard">Tirer une carte	</label>
 			</td>
 
 			<td>
-				<input type="radio" name="radioChoice" id="past" value="past" class="past">	
+				<input type="radio" name="radioChoice" id="past" value="past" class="past" required>	
 				<label name="past">Passer la main</label>
 			</td>
 		</tr>
 		<tr class="trTableChoice2">
 			<td>
-				<input type="radio" name="radioChoice" id="double" value="double" class="double">
+				<input type="radio" name="radioChoice" id="double" value="double" class="double" required>
 				<label name="double">Doubler la mise</label>
 			</td>
 
 			<td>
-				<input type="radio" name="radioChoice" id="doubleGame" value="doubleGame" class="doubleGame">
+				<input type="radio" name="radioChoice" id="doubleGame" value="doubleGame" class="doubleGame" required>
 				<label name="doubleGame">Double son jeu</label>
 			</td>
 		</tr>
